ae585d5146b8f16d450335ad2cf3969bb4c86226
Domonkos Lezsák <domonkos@lezsak.hu>
2023-04-29 12:07:15 +0200

    Initial commit

---
+++ init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyD498PMUwH9Aqexcml9dllaLZto4OsZsiM",
+    authDomain: "lakoparki-eszkozok.firebaseapp.com",
+    projectId: "lakoparki-eszkozok",
+    storageBucket: "lakoparki-eszkozok.appspot.com",
+    messagingSenderId: "937734431748",
+    appId: "1:937734431748:web:da7baa6df275a6cdab0151"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
