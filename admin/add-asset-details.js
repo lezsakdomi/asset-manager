3c26bdd7187c5149d595e5821283c2bdac6f9631
Domonkos Lezsák <domonkos@lezsak.hu>
2023-05-01 14:04:25 +0200

    Some improvements in admin scripts

--- admin/add-asset-details.js
+++ admin/add-asset-details.js
@@ -1,19 +1,43 @@
 import "dotenv";
 import admin from "firebase-admin";
 import dotenv from "dotenv";
+import {readFile} from "fs/promises";
+import {ulid} from "ulid";
 
-dotenv.config()
+dotenv.config();
 
 admin.initializeApp();
 
 const firestore = admin.firestore();
-for (const assetRef of await firestore.collection('/assets').listDocuments()) {
-    console.log(assetRef.id)
-    let name = assetRef.id;
+const auth = admin.auth();
+for (const asset of JSON.parse(await readFile("assets.json", 'utf-8'))) {
+    let {baseUser: baseUid, description, name} = asset;
+    console.log(name);
+    const id = name;
+    if (baseUid === "???") {
+        baseUid = '_unknown';
+    } else if (baseUid === "Forrási tér") {
+        baseUid = null;
+    } else if (baseUid && !baseUid.startsWith('?')) {
+        const qss = await firestore.collection('users').where('displayName', '==', baseUid).get();
+        const [dss] = qss.docs;
+        if (dss) {
+            baseUid = dss.id;
+        } else {
+            const newUid = '_fake_' + ulid();
+            await firestore.collection('users').doc(newUid).set({
+                displayName: baseUid
+            });
+            baseUid = newUid;
+        }
+    } else {
+        console.warn(baseUid, "is not a valid user name")
+        baseUid = '_unknown';
+    }
     name = name[0].toUpperCase() + name.slice(1);
     name = name.replace(/\s+\d+$/, '');
-    await assetRef.update({
-        name,
-        available: true,
-    })
+    description = description || null;
+    await firestore.collection('/assets').doc(id).set({
+        name, description, baseUid, available: true
+    });
 }
--- admin/update-users.js
+++ admin/update-users.js
@@ -1,6 +1,7 @@
 import dotenv from "dotenv";
 import admin from "firebase-admin";
 import {promises as rlp} from "readline";
+import {usersByEmail} from "./users.js";
 
 dotenv.config();
 const rl = rlp.createInterface({
@@ -12,30 +13,57 @@ admin.initializeApp();
 const auth = admin.auth();
 const firestore = admin.firestore();
 const {users} = await auth.listUsers();
-for (const user of users) {
+for (let user of users) {
     console.log(user.email);
-    let becameMember = false;
-    if (!user.customClaims.member) {
-        if ((await rl.question("User is not a member. Should we set it as member? [y/N] ")).toLowerCase() === 'y') {
-            await auth.setCustomUserClaims(user.uid, {member: true});
-            becameMember = true;
-        }
+    if (usersByEmail[user.email]) {
+        const {displayName, phoneNumber, member} = usersByEmail[user.email];
+        user = await auth.updateUser(user.uid, {
+            displayName, phoneNumber
+        });
     }
-    if (user.customClaims.member || becameMember) {
-        if (!user.phoneNumber) {
-            const phoneNumber = await rl.question("User has no phone number set. Enter here, or leave empty to skip: ");
-            if (phoneNumber) {
-                await auth.updateUser(user.uid, {phoneNumber});
-            }
-        }
-        await firestore.collection('users').doc(user.uid).set(
-            Object.entries(JSON.parse(JSON.stringify(user, (k, v) => v === undefined ? null : v)))
+    await auth.setCustomUserClaims(user.uid, {
+        member: null
+    });
+    const userRef = firestore.doc(`/users/${user.uid}`);
+    if (!(await userRef.get()).exists) {
+        if ((await rl.question("User is not found in database. Should we import it? [y/N] ")).toLowerCase() === 'y') {
+            userRef.set(Object.entries(JSON.parse(JSON.stringify(user, (k, v) => v === undefined ? null : v)))
                 .filter(([k, v]) => !k.startsWith('password'))
+                .filter(([k, v]) => k !== 'customClaims')
                 .reduce((a, [k, v]) => ({...a, [k]: v}), {}));
-    } else {
-        if ((await rl.question("Should we delete it? [y/N] ")).toLowerCase() === 'y') {
-            await auth.deleteUser(user.uid);
+        } else {
+            continue;
         }
     }
+    if (usersByEmail[user.email]) {
+        const {displayName, phoneNumber, member} = usersByEmail[user.email];
+        await userRef.update({
+            'customClaims.member': member,
+            displayName: displayName || null,
+            phoneNumber: phoneNumber || null,
+        });
+    }
+    // if (!user.customClaims.member) {
+    //     if ((await rl.question("User is not a member. Should we set it as member? [y/N] ")).toLowerCase() === 'y') {
+    //         await auth.setCustomUserClaims(user.uid, {member: true});
+    //         becameMember = true;
+    //     }
+    // }
+    // if (user.customClaims.member || becameMember) {
+    //     if (!user.phoneNumber) {
+    //         const phoneNumber = await rl.question("User has no phone number set. Enter here, or leave empty to skip: ");
+    //         if (phoneNumber) {
+    //             await auth.updateUser(user.uid, {phoneNumber});
+    //         }
+    //     }
+    //     await firestore.collection('users').doc(user.uid).set(
+    //         Object.entries(JSON.parse(JSON.stringify(user, (k, v) => v === undefined ? null : v)))
+    //             .filter(([k, v]) => !k.startsWith('password'))
+    //             .reduce((a, [k, v]) => ({...a, [k]: v}), {}));
+    // } else {
+    //     if ((await rl.question("Should we delete it? [y/N] ")).toLowerCase() === 'y') {
+    //         await auth.deleteUser(user.uid);
+    //     }
+    // }
 }
-rl.close()
+rl.close();
