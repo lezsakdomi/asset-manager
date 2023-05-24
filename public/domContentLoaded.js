cf1cf53459024ce41a5a1b4770e41685c0d41fd8
Domonkos Lezsák <domonkos@lezsak.hu>
2023-05-24 18:17:01 +0200

    Consider DOM loaded after 3s anyway

--- public/domContentLoaded.js
+++ public/domContentLoaded.js
@@ -1,3 +1,4 @@
-export default new Promise((resolve) => {
-    document.addEventListener('DOMContentLoaded', resolve)
-})
+export default Promise.race([new Promise((resolve) => {
+    document.addEventListener('DOMContentLoaded', resolve);
+})], new Promise(resolve => setTimeout(resolve, 3000)));
+// todo better to check for DOM state or sth
