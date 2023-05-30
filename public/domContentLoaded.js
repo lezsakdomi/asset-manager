e6128be36066388f2bd3d3e22a91874f9668387e
Domonkos Lezsák <domonkos@lezsak.hu>
2023-05-30 23:02:10 +0200

    Fix DOMContentLoaded promise
    Mainly affecting iPhone devices

--- public/domContentLoaded.js
+++ public/domContentLoaded.js
@@ -1,4 +1,7 @@
-export default Promise.race([new Promise((resolve) => {
-    document.addEventListener('DOMContentLoaded', resolve);
-})], new Promise(resolve => setTimeout(resolve, 3000)));
-// todo better to check for DOM state or sth
+export default new Promise((resolve) => {
+    if (document.readyState === 'complete') {
+        resolve();
+    } else {
+        document.addEventListener('DOMContentLoaded', resolve);
+    }
+});
